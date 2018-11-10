f1129ab82dfe3c97b99f22d9c8561e525ebfa472
Anat <anat.eliahu@gmail.com>
2018-11-10 20:57:15 +0200

    Create Layout component base

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Layout from '../src/components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Some text
+        <Layout>
+          <p>Test</p>
+        </Layout>
       </div>
     );
   }
