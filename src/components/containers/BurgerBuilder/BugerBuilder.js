026b9f5b49b42ef2e71645f457b61439ffd5b643
anate <anat.eliahu@gmail.com>
2018-12-01 19:12:22 +0200

    Temp commit

--- src/components/containers/BurgerBuilder/BugerBuilder.js
+++ src/components/containers/BurgerBuilder/BugerBuilder.js
@@ -1,12 +1,67 @@
 import React, {Component, Fragment} from 'react';
+import Burger from "../../../components/Burger/Burger";
+import BuildControls from "../../Burger/BuildControls/BuildControls";
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+}
 class BurgerBuilder extends Component {
+
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    },
+    totalPrice: 4 // the base price
+  }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {...this.state.ingredients, [type]: updatedCount}
+    const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+    this.setState({
+      totalPrice: newPrice,
+      ingredients: updatedIngredients
+    })
+  }
+
+  removeIngredientHandler = (type) => {
+    if (this.state.ingredients[type] !== 0) {
+      const newPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
+      const updatedIngredients = {
+          ...this.state.ingredients,
+        [type]: this.state.ingredients[type] - 1
+      }
+      this.setState({
+        totalPrice: newPrice,
+        ingredients: updatedIngredients
+      })
+    }
+  }
+
   render() {
+    const disabledInfo = {
+        ...this.state.ingredients
+    }
+
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] === 0;
+    }
+
     return (
-      <Fragment>
-        <div>Burger</div>
-        <div>Build Controls</div>
-      </Fragment>
+        <Fragment>
+          <Burger ingredients={this.state.ingredients} />
+          <BuildControls ingredientAdded={this.addIngredientHandler}
+                         ingredientRemoved={this.removeIngredientHandler}
+                         disabled={disabledInfo}
+                         price={this.state.totalPrice}/>
+        </Fragment>
     );
   }
 }
